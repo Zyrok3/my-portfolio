a83a408606b72ffd50a8272fa643a29116935e5d
Felix Reder <felix.reder@gmail.com>
2025-07-26 22:20:40 +0200

    feat: added certificates data

---
+++ src/data/certificates.ts
@@ -0,0 +1,23 @@
+export interface Certificate {
+  title: string;
+  icon: string; // path to certificate icon
+}
+
+export const certificates: Certificate[] = [
+  // {
+  //   title: "AWS Certified Cloud Practitioner",
+  //   icon: "/icons/aws-certificate.svg",
+  // },
+  // {
+  //   title: "Cisco CCNA",
+  //   icon: "/icons/cisco-certificate.svg",
+  // },
+  // {
+  //   title: "Google IT Support",
+  //   icon: "/icons/google-certificate.svg",
+  // },
+  // {
+  //   title: "Coursera Deep Learning",
+  //   icon: "/icons/coursera-certificate.svg",
+  // },
+];
