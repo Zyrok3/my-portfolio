ad6df90aaca3be6e2f1a110a22c93c05f97c6dcd
Felix Reder <felix.reder@gmail.com>
2025-07-26 22:17:40 +0200

    feat: add global styles and AOS animation setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,41 @@
+import { useEffect } from "react";
 import Navbar from "./components/Navbar";
-import Footer from "./components/Footer"
+import Footer from "./components/Footer";
+import Projects from "./sections/Projects";
+import HeroSection from "./sections/HeroSection";
+import Awards from "./sections/Awards";
+import Experiences from "./sections/Experiences";
+import Toolkit from "./sections/Toolkit";
+import AboutMe from "./sections/AboutMe";
+// Import AOS
+import AOS from "aos";
+import "aos/dist/aos.css";
+import SocialLinks from "./components/SocialLinks";
 
-function App(){
-    return (
-        <>
-            <Navbar />
-            <Footer />
-        </>
-    );
+function App() {
+  useEffect(() => {
+    // Initialize AOS
+    AOS.init({
+      duration: 800,
+      easing: "ease-in-out",
+      once: true,
+      mirror: false,
+    });
+  }, []);
+
+  return (
+    <>
+      <SocialLinks />
+      <Navbar />
+      <HeroSection />
+      <AboutMe />
+      <Projects />
+      <Awards />
+      <Experiences />
+      <Toolkit />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
-
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./styles/globals.css";
+import "./styles/tailwind.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
