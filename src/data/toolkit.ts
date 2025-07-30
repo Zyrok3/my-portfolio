0c7716890b54597ad3d41cc1121199b6312d3dd1
Felix Reder <reder.f@ikarus.at>
2025-07-30 16:38:09 +0200

    feat: Add SVG icons for various technologies including Django, Docker, Flutter, GitHub, Java, JavaScript, Jira, Notion, Python, React, Tailwind CSS, TypeScript, and Vercel

--- src/data/toolkit.ts
+++ src/data/toolkit.ts
@@ -1,25 +1,23 @@
 export interface ToolkitItem {
   name: string;
-  icon: string;
+  icon: string; // path to SVG image
 }
 
 export const toolkit: ToolkitItem[] = [
-  { name: 'Java',          icon: '/icons/html5.svg' },
-  { name: 'Python',        icon: 'fab fa-python' },
-  { name: 'HTML5',         icon: 'fab fa-html5' },
-  { name: 'CSS3',          icon: '/icons/CSS3.svg' },
-  { name: 'JavaScript',    icon: 'fab fa-js' },
-  { name: 'TypeScript',    icon: 'fas fa-file-code' },
-  { name: 'React',         icon: 'fab fa-react' },
-  { name: 'Tailwind-CSS',  icon: 'fab fa-tailwind' },
-  { name: 'Spring-Boot',   icon: 'fas fa-leaf' },
-  { name: 'Django',        icon: 'fas fa-feather' },
-  { name: 'MariaDB',       icon: 'fas fa-database' },
-  { name: 'Docker',        icon: 'fab fa-docker' },
-  { name: 'GitHub',        icon: 'fab fa-github' },
-  { name: 'Bash/Shell',    icon: 'fas fa-terminal' },
-  { name: 'Vercel',        icon: 'fab fa-vercel' },
-  { name: 'Flutter',       icon: 'fab fa-flutter' },
-  { name: 'Jira',          icon: 'fab fa-jira' },
-  { name: 'Notion',        icon: 'fab fa-notion' },
+  { name: 'Java',          icon: '/icons/java.svg' },
+  { name: 'Python',        icon: '/icons/python.svg' },
+  { name: 'HTML5',         icon: '/icons/html5.svg' },
+  { name: 'CSS3',          icon: '/icons/css3.svg' },
+  { name: 'JavaScript',    icon: '/icons/javascript.svg' },
+  { name: 'TypeScript',    icon: '/icons/typescript.svg' },
+  { name: 'React',         icon: '/icons/react.svg' },
+  { name: 'Tailwind-CSS',  icon: '/icons/tailwindcss.svg' },
+  { name: 'Django',        icon: '/icons/django.svg' },
+  { name: 'Docker',        icon: '/icons/docker.svg' },
+  { name: 'GitHub',        icon: '/icons/github.svg' },
+  { name: 'Bash/Shell',    icon: '/icons/bash.svg' },
+  { name: 'Vercel',        icon: '/icons/vercel.svg' },
+  { name: 'Flutter',       icon: '/icons/flutter.svg' },
+  { name: 'Jira',          icon: '/icons/jira.svg' },
+  { name: 'Notion',        icon: '/icons/notion.svg' },
 ];
